8855f34b7c3452c604b3bdf517f057f86a468f90
cube <110179449+cube786@users.noreply.github.com>
2024-06-11 19:42:38 +0530

    Add files via upload

---
+++ CaesarCipher/main.py
@@ -0,0 +1,33 @@
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(plain_text, shift_amount, direction_value):
+    end_text = ""
+    if direction_value == 'decode':
+        shift_amount *= -1
+    for char in plain_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {direction_value}d text is {end_text}")
+
+
+game_on = True
+while game_on:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(plain_text=text, shift_amount=shift, direction_value=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart == 'no':
+        game_on = False
